f7187041cba624dceca240ae0f6a71f4872b2b8f
mohammad rtr <assd13@gmail.com>
2024-05-26 12:20:59 -0700

    compleat if & else

--- js/app.js
+++ js/app.js
@@ -1,6 +1,27 @@
 let age = prompt('How old are you') ;
 
-if(age < 18){
+//if(age < 18){
 
-    alert('you are too young') ;
-}
+  //  alert('you are too young') ;
+//}
+
+//else if(age >55){
+
+  //  alert('you are too old') ;
+//}  
+
+//else{
+  //  alert('you have full accses') ;
+//}
+
+//if(age < 18 || age > 55){
+
+  //  alert("you can't enter in this site") ;
+//}
+
+//else{
+  //  alert("you have full accses") ;
+//}
+
+
+
